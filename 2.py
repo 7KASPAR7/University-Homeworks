3929c0f1f8ca7aa6c940e1fb4aaa5da4c1946c1b
7KASPAR7 <RybakovS.V@yandex.ru>
2018-09-28 21:37:35 +0300

    3.0

--- 2.py
+++ 2.py
@@ -4,9 +4,11 @@ def calc(operand1, operation, operand2):
        elif operation=='-':
            return operand1-operand2
        elif operation=='/':
-           return operand1/operand2
+           if operand2==0:
+               raise ZeroDivisionError('Деление на ноль')
+           else:return operand1/operand2
        elif operation=='*':
            return operand1*operand2
        else:
            raise ValueError('Неизвестная операция')
-print(calc(5, '*', 3))
+print(calc(3, '*', 80))
