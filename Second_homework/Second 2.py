71118d3fbc4fd232886d5aadd12c5db5c6d357bb
7KASPAR7 <RybakovS.V@yandex.ru>
2018-10-11 23:40:04 +0300

    Update Second_homework/Second 2.py

--- Second_homework/Second 2.py
+++ Second_homework/Second 2.py
@@ -5,29 +5,29 @@ def perfect(a):
     if a!=a//1:
         result='Требуется целое число'
     elif a<=0:
-        result='Несовершенное число'
+        result='Требуется неотрицательное число'
     else:
         S=0
         for i in range(1,a):
              if a%i==0:
                  S=S+i
         if a==S:
-             result='Совершенное число'
+             result=True
         else:
-             result='Несовершенное число'
+             result=False
     return result
 print(perfect(8128))
 
 class PerfectTest(TestCase):
     def test_valid_values(self):
-        self.assertEqual(perfect(6), 'Совершенное число')
-        self.assertEqual(perfect(0), 'Несовершенное число')
-        self.assertEqual(perfect(1), 'Несовершенное число')
-        self.assertEqual(perfect(-6), 'Несовершенное число')
-        self.assertEqual(perfect(-25), 'Несовершенное число')
-        self.assertEqual(perfect(28), 'Совершенное число')
-        self.assertEqual(perfect(8128), 'Совершенное число')
-        self.assertEqual(perfect(8), 'Несовершенное число')
+        self.assertTrue(perfect(6))
+        self.assertEqual(perfect(0),'Требуется неотрицательное число')
+        self.assertFalse(perfect(1))
+        self.assertEqual(perfect(-6),'Требуется неотрицательное число')
+        self.assertEqual(perfect(-25),'Требуется неотрицательное число')
+        self.assertTrue(perfect(28))
+        self.assertTrue(perfect(8128))
+        self.assertFalse(perfect(8))
         self.assertEqual(perfect(8.3), 'Требуется целое число')
         self.assertEqual(perfect(-2.4), 'Требуется целое число')
 main()
