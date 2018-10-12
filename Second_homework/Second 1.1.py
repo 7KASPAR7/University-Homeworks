c029151f70aa19576d499366730a886f73e88293
7KASPAR7 <RybakovS.V@yandex.ru>
2018-10-12 21:48:12 +0300

    123

--- Second_homework/Second 1.1.py
+++ Second_homework/Second 1.1.py
@@ -1,6 +1,8 @@
 from unittest import TestCase, main
+import time
+from memory_profiler import profile
 
-
+@profile
 def factorial(n):
     if n<0 or n!=n//1:
        F='Факториал определен только для целых неотрицательных чисел'
@@ -9,8 +11,9 @@ def factorial(n):
         for i in range(1,n+1):
             F=F*i
     return F
+start_time=time.time()
 print(factorial(8))
-
+print(time.time()-start_time)
 class PerfectTest(TestCase):
     def test_valid_values(self):
         self.assertEqual(factorial(6), 720)
@@ -21,3 +24,6 @@ class PerfectTest(TestCase):
         self.assertEqual(factorial(8.5), 'Факториал определен только для целых неотрицательных чисел')
 
 main()
+start_time = time.time(factorial(8))
+main()
+print("--- %s seconds ---" % (time.time(factorial(8)) - start_time))
---
+++ Second_homework/Second 3.py
@@ -0,0 +1,37 @@
+from unittest import TestCase, main
+
+def factorial(n):
+    if n<0 or n!=n//1:
+       F='Некорректный ввод данных'
+    else:
+        F=1
+        for i in range(1,n+1):
+            F=F*i
+    return F
+
+def Pascal(n):
+    if n<=0 or n//1!=n:
+        return 'Некорректный ввод данных'
+    else:
+        RESULT=[[1]]
+        for i in range(1,n):
+            result=[]
+            for l in range(0,i+1):
+                a=factorial(i)/(factorial(l)*factorial(i-l))
+                a=int(a)
+                result.append(a)
+            RESULT.append(result)
+        return RESULT
+Pascal(6)
+
+
+class PerfectTest(TestCase):
+    def test_valid_values(self):
+        self.assertEqual(Pascal(4), [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]])
+        self.assertEqual(Pascal(1), [[1]])
+        self.assertEqual(Pascal(0), 'Некорректный ввод данных')
+        self.assertEqual(factorial(-6), 'Некорректный ввод данных')
+        self.assertEqual(factorial(-2.5), 'Некорректный ввод данных')
+        self.assertEqual(factorial(8.5), 'Некорректный ввод данных')
+
+main()
