480e22afa8daad1df2c3e3605c0abf18798b602e
7KASPAR7 <RybakovS.V@yandex.ru>
2018-10-13 10:35:31 +0300

    123

--- Second_homework/Second 1.1.py
+++ Second_homework/Second 1.1.py
@@ -5,27 +5,25 @@ from memory_profiler import profile
 
 def factorial(n):
     if n<0 or n!=n//1:
-       F='Факториал определен только для целых неотрицательных чисел'
+       raise ValueError('Факториал определен только для целых неотрицательных чисел')
     else:
         F=1
         for i in range(1,n+1):
             F=F*i
     return F
-
 @profile
 def timer(n):
     factorial(n)
 start_time=time.time()
 timer(800)
 print('time:',time.time()-start_time)
-
 class PerfectTest(TestCase):
     def test_valid_values(self):
         self.assertEqual(factorial(6), 720)
         self.assertEqual(factorial(0), 1)
         self.assertEqual(factorial(1), 1)
-        self.assertEqual(factorial(-6), 'Факториал определен только для целых неотрицательных чисел')
-        self.assertEqual(factorial(-2.5), 'Факториал определен только для целых неотрицательных чисел')
-        self.assertEqual(factorial(8.5), 'Факториал определен только для целых неотрицательных чисел')
+        self.assertRaises( ValueError, factorial, -6)
+        self.assertRaises( ValueError, factorial, -2.5)
+        self.assertRaises( ValueError, factorial, 8.5)
 
 main()
--- Second_homework/Second 1.2.py
+++ Second_homework/Second 1.2.py
@@ -4,28 +4,26 @@ from memory_profiler import profile
 
 def factorial(n):
     if n<0 or n!=n//1:
-       F='Факториал определен только для целых неотрицательных чисел'
+        raise ValueError('Факториал определен только для целых неотрицательных чисел')
     else:
         if n==0:
             F=1
         else:
             F=factorial(n-1)*n
     return F
-
 @profile
 def timer(n):
     factorial(n)
 start_time=time.time()
 timer(800)
 print('time:',time.time()-  start_time)
-
 class PerfectTest(TestCase):
     def test_valid_values(self):
         self.assertEqual(factorial(6), 720)
         self.assertEqual(factorial(0), 1)
         self.assertEqual(factorial(1), 1)
-        self.assertEqual(factorial(-6), 'Факториал определен только для целых неотрицательных чисел')
-        self.assertEqual(factorial(-2.5), 'Факториал определен только для целых неотрицательных чисел')
-        self.assertEqual(factorial(8.5), 'Факториал определен только для целых неотрицательных чисел')
+        self.assertRaises(ValueError, factorial, -6)
+        self.assertRaises(ValueError, factorial, -2.5)
+        self.assertRaises(ValueError, factorial, 8.5)
 
 main()
