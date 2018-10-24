96d918325fe001cf21ad656d5f74cc5149c82dc4
7KASPAR7 <RybakovS.V@yandex.ru>
2018-10-24 22:16:38 +0300

    123

--- Third_homework/Cesar.py
+++ Third_homework/Cesar.py
@@ -1,21 +1,22 @@
-def cesar(string,code):
-        L='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        l='abcdefghijklmnopqrstuvwxyz'
-        result=''
-        for i in range(0,len(string)):
-            Letter=string[i]
+def cesar(string, code):
+        LETTER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        letter = 'abcdefghijklmnopqrstuvwxyz'
+        result = ''
+        for i in range(0, len(string)):
+            symbol = string[i]
             for k in range(0, 26):
-                if k+code>26 or k+code<0:
-                    index=(k+code)%26
-                if string[i]==L[k]:
-                    Letter=L[index]
-                elif string[i]==l[k]:
-                    Letter=l[index]
-            result=result+Letter
+                if k+code > 26 or k+code < 0:
+                    index = (k+code) % 26
+                if string[i] == LETTER[k]:
+                    symbol = LETTER[index]
+                elif string[i] == letter[k]:
+                    symbol = letter[index]
+            result = result+symbol
         return result
 
-a=str(input())
-n=input()
-if type(n)!=int:
+
+a = str(input())
+n = input()
+if type(n) != int:
     raise TypeError('Неверный ввод данных')
-cesar(a,n)
+cesar(a, n)
