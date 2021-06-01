fcda0d3d2b22ac2cd2eb7e0c142975fd87a90560
Рыбаков Сергей <43292221+7KASPAR7@users.noreply.github.com>
2021-06-01 14:04:54 +0300

    Delete Kroko directory

--- Kroko/GameWindow.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-
-pygame.init()
-pygame.mixer.init()  # для звука
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
